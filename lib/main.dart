e2bff0981664872630edf5f699cd5e04de18e447
Richard <rdobmk@gmail.com>
2024-08-10 13:45:44 +0200

    Create lists with different types of items

--- lib/main.dart
+++ lib/main.dart
@@ -18,20 +18,65 @@ class MyApp extends StatelessWidget {
             title: const Text(appTitle),
           ),
           // #docregion add-widget
-          body: GridView.count(
-            // Create a grid with 2 columns. If you change the scrollDirection
-            // to horizontal, this produces 2 rows.
-            crossAxisCount: 3,
-            // Generate 100 widgets that display their index in the List.
-            children: List.generate(50, (index) {
-              return Center(
-                child: Text(
-                  'Item ${index}',
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
+          body: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index];
+
+              return ListTile(
+                title: item.buildTitle(context),
+                subtitle: item.buildSubtitle(context),
               );
-            }),
+            },
           )),
     );
   }
 }
+
+/// The base class for the different types of items the list can contain.
+abstract class ListItem {
+  /// The title line to show in a list item.
+  Widget buildTitle(BuildContext context);
+
+  /// The subtitle line, if any, to show in a list item.
+  Widget buildSubtitle(BuildContext context);
+}
+
+/// A ListItem that contains data to display a heading.
+class HeadingItem implements ListItem {
+  final String heading;
+
+  HeadingItem(this.heading);
+
+  @override
+  Widget buildTitle(BuildContext context) {
+    return Text(
+      heading,
+      style: Theme.of(context).textTheme.headlineSmall,
+    );
+  }
+
+  @override
+  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
+}
+
+/// A ListItem that contains data to display a message.
+class MessageItem implements ListItem {
+  final String sender;
+  final String body;
+
+  MessageItem(this.sender, this.body);
+
+  @override
+  Widget buildTitle(BuildContext context) => Text(sender);
+
+  @override
+  Widget buildSubtitle(BuildContext context) => Text(body);
+}
+
+final items = List<ListItem>.generate(
+  1000,
+  (i) => i % 6 == 0
+      ? HeadingItem('Heading $i')
+      : MessageItem('Sender $i', 'Message body $i'),
+);
